163ed4432f624d63c4e1bde416bd9f900bd03636
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-09-04 14:33:39 +1000

    feat(components): inject wallet wrapper into lookup component

--- client-applications/client/src/components/lookup.js
+++ client-applications/client/src/components/lookup.js
@@ -1,6 +1,7 @@
 import { customElement } from 'aurelia-framework';
+import { WalletHttpWrapper } from '../services/wallet-http-wrapper';
 
 @customElement('lookup')
 export class Lookup {
-
+    static inject = [WalletHttpWrapper];
 }
